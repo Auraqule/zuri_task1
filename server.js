10a5e5fb2d0d50c4baa7d95c6e8d99858bd17681
auraqule <auraqule@gmail.com>
2022-10-29 01:12:32 +0100

    Backend Task 1 Hng9_Zuri with NodeJs

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+// APP CONFIG
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+const messageBody = {
+  slackUsername: "Auraqule",
+  backend: true,
+  age: 25,
+  bio: "My name is Christian Peters, and I am a Backend Engineer ☺",
+};
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// DB CONFIG
+
+// API ROUTES/ENDPOINTS
+
+app.get("/", (req, res) => {
+  res.status(200).send({ ...messageBody });
+});
+
+// LISTERNER
+app.listen(PORT, () => console.log(`Listening on Localhost:${PORT}`));
